da04b5765af6f78c41cbb68c4604885471c41740
Viktor <viktor@v.v>
2026-01-28 22:02:03 +0700

    up

--- TowerClimber/Views/GameView.swift
+++ TowerClimber/Views/GameView.swift
@@ -326,44 +326,6 @@ struct StarIcon: View {
     }
 }
 
-struct Star: Shape {
-    func path(in rect: CGRect) -> Path {
-        let center = CGPoint(x: rect.midX, y: rect.midY)
-        let outerRadius = min(rect.width, rect.height) / 2
-        let innerRadius = outerRadius * 0.4
-        let points = 5
-        
-        var path = Path()
-        
-        for i in 0..<points * 2 {
-            let radius = i.isMultiple(of: 2) ? outerRadius : innerRadius
-            let angle = Double(i) * .pi / Double(points) - .pi / 2
-            let point = CGPoint(
-                x: center.x + CGFloat(cos(angle)) * radius,
-                y: center.y + CGFloat(sin(angle)) * radius
-            )
-            
-            if i == 0 {
-                path.move(to: point)
-            } else {
-                path.addLine(to: point)
-            }
-        }
-        
-        path.closeSubpath()
-        return path
-    }
-}
-
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.closeSubpath()
-        return path
-    }
 }
 
 #Preview {
